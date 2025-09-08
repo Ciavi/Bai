70a88370dbb12c0e34ec7b1ee14881d4ec748a49
Ciavi <13613469+Ciavi@users.noreply.github.com>
2025-09-08 10:05:32 +0200

    Accountant class

--- system/accountant.py
+++ system/accountant.py
@@ -1,6 +1,9 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import ssl
 
+from discord.ext.commands import Bot
+
+
 class WebhookHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         if self.path == "/ko-fi":
@@ -8,10 +11,18 @@ class WebhookHandler(BaseHTTPRequestHandler):
 
         self.send_response(200)
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile="./cert.pem", keyfile="./key.pem")
-context.check_hostname = False
 
-with HTTPServer(("localhost", 4443), WebhookHandler) as httpd:
-    httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
-    httpd.serve_forever()
+class Accountant:
+    def __init__(self, bot: Bot):
+        self.bot = bot
+
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        context.load_cert_chain(certfile="./cert.pem", keyfile="./key.pem")
+        context.check_hostname = False
+
+        self.context = context
+
+        with HTTPServer(("0.0.0.0", 4443), WebhookHandler) as httpd:
+            httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+            httpd.serve_forever()
+
